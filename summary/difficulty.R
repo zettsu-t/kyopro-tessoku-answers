1bc6935be1907ce631992f25278e88c8a7f6fc8e
zettsu-t <zettsu.tatsuya@gmail.com>
2023-08-05 10:04:23 +0100

    Summarize results

---
+++ summary/difficulty.R
@@ -0,0 +1,222 @@
+library(extrafont)
+library(jsonlite)
+library(tidyverse)
+
+g_font_name <- "Migu 1M"
+rating_chars <- c("灰", "茶", "緑", "水", "青", "黄", "橙", "赤")
+rating_colors <- c(
+  "#808080", "#804000", "#008000", "#00C0C0",
+  "#0000FF", "#C0C000", "#FF8000", "#FF0000"
+)
+g_incoming_data_dir <- "incoming_data"
+
+## 問題を読み込む
+read_task_difficulties <- function(path) {
+  contests <- jsonlite::read_json(path)
+
+  purrr::reduce(.x = seq_len(NROW(contests)), .init = NULL, .f = function(acc, i) {
+    tasks <- contests[i]
+    name <- names(tasks)
+    df <- as_tibble(do.call(rbind.data.frame, tasks)) %>%
+      dplyr::mutate(contest = name, .before = 1)
+    if (is.null(acc)) {
+      df
+    } else {
+      dplyr::bind_rows(acc, df)
+    }
+  })
+}
+
+## コンテストを限定する
+select_contests <- function(df_contest, contest_name, out_dirname) {
+  inner_name <- stringr::str_to_lower(contest_name)
+  pattern <- paste0("^(", inner_name, ")(.*)")
+
+  df_target <- df_contest %>%
+    tidyr::extract(col = contest, into = c("contest", "task"), regex = "(.*)_([^_]+$)") %>%
+    dplyr::filter(stringr::str_starts(stringr::str_to_lower(contest), inner_name)) %>%
+    tidyr::extract(col = contest, into = c("name", "id"), regex = pattern, remove = FALSE) %>%
+    dplyr::mutate(name = stringr::str_to_lower(name))
+
+  tasks <- c("a", "b", "c", "d", "e", "f", "g", "h")
+  df_tasks <- df_target %>%
+    dplyr::select(all_of(c("contest", "task", "difficulty"))) %>%
+    na.omit() %>%
+    dplyr::filter(task %in% tasks) %>%
+    dplyr::arrange(contest, task) %>%
+    dplyr::mutate(contest = stringr::str_to_upper(contest)) %>%
+    dplyr::mutate(rating = 1 + pmax(0, floor(difficulty / 400))) %>%
+    dplyr::mutate(mark = ifelse(rating < 10, paste(rating), NA)) %>%
+    dplyr::select(-all_of(c("difficulty", "rating"))) %>%
+    tidyr::pivot_wider(id_cols = "contest", names_from = "task", values_from = "mark") %>%
+    dplyr::mutate(across(everything(), ~ replace_na(.x, " "))) %>%
+    dplyr::mutate(sep = ":", .after = contest) %>%
+    tidyr::unite("tasks", everything(), sep = "", remove = FALSE, na.rm = FALSE) %>%
+    dplyr::mutate(tasks = stringr::str_trim(tasks, side = "right"))
+
+  if (!is.na(out_dirname)) {
+    out_filename <- file.path(out_dirname, paste0("difficulty_auto_", contest_name, ".csv")) %>%
+      stringr::str_to_lower()
+    df_tasks %>%
+      dplyr::select(tasks) %>%
+      readr::write_csv(file = out_filename, col_names = FALSE)
+  }
+
+  df_target
+}
+
+## 結果を読み込む
+read_results <- function(path, contest_name) {
+  name <- stringr::str_to_lower(contest_name)
+  cin <- file(path, "r")
+  lines <- readLines(cin)
+  close(cin)
+
+  purrr::reduce(.x = lines, .init = NULL, .f = function(acc_all, line) {
+    # 解けたかどうかを記したテキストファイルを読む
+    # 1行1コンテストに対応する。コンテスト名(もしあれば)三桁のID, ':', A..H問題の結果を一行に書く。例えば
+    # ABC277:  ++-
+    # はコンテストABC277のA,Bは解いていない、C,Dは解けた、Eは解けなかった、F以降は解いていない
+    # ことを示す。+は解けた、空白は解いていない、それ以外は解けなかったことを示す
+    # :の直後にA問題の結果を置く。:と結果の間には何も書かない。
+    matched <- stringr::str_match(line, "^[\\D]*(\\d{3}):(.{1,8})")
+    if (is.na(matched[1, 1])) {
+      acc_all
+    } else {
+      id <- matched[1, 2]
+      results <- strsplit(matched[1, 3], "")[[1]]
+      df_contest <- purrr::reduce(.x = seq_len(NROW(results)), .init = NULL, .f = function(acc_contest, i) {
+        result <- results[i]
+        task <- c("a", "b", "c", "d", "e", "f", "g", "h")[i]
+        score <- NA
+        if (result == "+") {
+          score <- 1
+        } else if (result != " ") {
+          score <- -1
+        }
+        df <- tibble::tibble(name = name, id = id, task = task, score = score)
+        if (is.null(acc_contest)) {
+          df
+        } else {
+          dplyr::bind_rows(acc_contest, df)
+        }
+      })
+
+      if (is.null(acc_all)) {
+        df_contest
+      } else {
+        dplyr::bind_rows(acc_all, df_contest)
+      }
+    }
+  })
+}
+
+## 問題と結果を統合する
+merge_score <- function(df_tasks, df_results) {
+  df_left <- df_tasks %>%
+    dplyr::mutate(id = as.numeric(id))
+
+  df_right <- df_results %>%
+    dplyr::mutate(id = as.numeric(id))
+
+  dplyr::inner_join(df_left, df_right) %>%
+    dplyr::filter(!is.na(score)) %>%
+    dplyr::filter(abs(score) == 1) %>%
+    dplyr::mutate(difficulty = pmax(0, difficulty)) %>%
+    dplyr::arrange(name, id, task) %>%
+    dplyr::mutate(rank = 1 + pmax(0, difficulty %/% 400), .after = difficulty)
+}
+
+## 解けたかどうかを散布図にする
+draw_scatter_plot <- function(df_score, contest_name) {
+  id_minmax <- range(df_score$id)
+  id_center <- (id_minmax[2] + id_minmax[1]) / 2.0
+  id_tick <- 1.4 / (id_minmax[2] - id_minmax[1])
+  df_drawn <- df_score %>%
+    dplyr::mutate(x = difficulty, y = score + (id - id_center) * id_tick)
+
+  rating_range <- 400
+  rating_min <- rating_range * (seq_len(NROW(rating_colors)) - 1)
+  rating_max <- rating_range * seq_len(NROW(rating_colors))
+  df_grade <- tibble(start = rating_min, end = rating_max, color = factor(seq_len(NROW(rating_colors))))
+
+  title <- paste0(contest_name, "を時間無制限で解けたかどうか")
+  g <- ggplot()
+  g <- g + geom_rect(data = df_grade, aes(xmin = start, xmax = end, ymin = -Inf, ymax = Inf, fill = color), fill = rating_colors)
+  g <- g + geom_point(data = df_drawn, aes(x = x, y = y), shape = 17, size = 3, color = "gray80")
+  g <- g + scale_y_continuous(breaks = c(-1, 1), labels = c("解けなかった", "解けた"))
+  g <- g + geom_hline(yintercept = 0)
+  g <- g + labs(title = title, x = "Difficulty", y = "解けたかどうか")
+  g <- g + theme_bw()
+  g <- g + theme(
+    text = element_text(family = g_font_name),
+    axis.text = element_text(family = g_font_name, size = 14),
+    axis.title.x = element_text(family = g_font_name, size = 16),
+    axis.title.y = element_blank(),
+    strip.text = element_text(family = g_font_name, size = 18),
+    plot.title = element_text(family = g_font_name, size = 18)
+  )
+  g
+}
+
+## Ratingごとの正解率をヒストグラムにする
+draw_histogram <- function(df_score, contest_name) {
+  labels <- c("解けた", "解けなかった")
+  df_drawn <- df_score %>%
+    dplyr::mutate(color = factor(labels[(3 - score) / 2])) %>%
+    dplyr::mutate(color = forcats::fct_relevel(color, labels)) %>%
+    dplyr::select(c("rank", "color"))
+
+  title <- paste0(contest_name, "を時間無制限で解けたかどうか")
+  g <- ggplot()
+  g <- g + geom_bar(data = df_drawn, aes(x = rank, fill = color), color = "black")
+  g <- g + scale_fill_manual(values = c("royalblue", "gray80"))
+  g <- g + scale_x_continuous(breaks = seq_len(NROW(rating_chars)), labels = rating_chars)
+  g <- g + labs(title = title, x = "Rating", y = "問題数")
+  g <- g + theme_bw()
+  g <- g + theme(
+    text = element_text(family = g_font_name),
+    axis.text = element_text(family = g_font_name, size = 14),
+    axis.title = element_text(family = g_font_name, size = 16),
+    strip.text = element_text(family = g_font_name, size = 18),
+    plot.title = element_text(family = g_font_name, size = 18),
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    legend.text = element_text(size = 16)
+  )
+  g
+}
+
+## コンテストを限定して結果を集計する
+execute_all <- function(result_filename, df_contests, contest_name, out_dirname) {
+  df_target <- select_contests(
+    df_contest = df_contests, contest_name = contest_name,
+    out_dirname = out_dirname
+  )
+  df_results <- read_results(result_filename, contest_name = contest_name)
+  df_score <- merge_score(df_tasks = df_target, df_results = df_results)
+
+  g_scatter <- draw_scatter_plot(df_score = df_score, contest_name = contest_name)
+  ggsave("images/score.png", plot = g_scatter, width = 6, height = 4)
+  g_histogram <- draw_histogram(df_score = df_score, contest_name = contest_name)
+  ggsave("images/hist.png", plot = g_histogram, width = 6, height = 4)
+
+  list(
+    df_target = df_target, df_results = df_results, df_score = df_score,
+    g_scatter = g_scatter, g_histogram = g_histogram
+  )
+}
+
+# 難易度をAtCoder Problemsからダウンロードする
+# https://github.com/kenkoooo/AtCoderProblems/blob/master/doc/api.md
+# からリンクされている
+# https://kenkoooo.com/atcoder/resources/problem-models.json
+# 何度もダウンロードしないように、解析はローカルのファイルを読む
+df_all_contests <- read_task_difficulties(file.path(g_incoming_data_dir, "problem-models.json"))
+
+# ABC (AtCoder Beginner Contest) に限る
+result <- execute_all(
+  result_filename = "results/results.txt",
+  df_contests = df_all_contests, contest_name = "ABC",
+  out_dirname = g_incoming_data_dir
+)
---
+++ summary/difficulty.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+# python3 difficulty.py
+
+import sys
+import re
+import argparse
+
+MAX_N_TASKS = 8
+MAX_DIFFICULTY = 9
+colors = ["?", "灰", "茶", "緑", "水", "青", "黄", "橙", "赤", "赤<"]
+
+# 1行1コンテストに対応する。
+# コンテスト名(もしあれば)三桁のID, ':', A..H問題の結果または難易度
+re_contest_pattern = "^([\\D]*\\d{3}):(.{0," + str(MAX_N_TASKS) + "})"
+re_id_pattern = "^[\\D]*(\\d{3})"
+
+def collect_results(lower_id, upper_id, difficulty_filename, result_filename):
+    difficulty_map = {}
+    with open(difficulty_filename) as f:
+        for line in f:
+            s = line.strip()
+            m = re.match(re_contest_pattern, s)
+            if not m:
+                continue
+            key = m.groups()[0]
+            value = m.groups()[1]
+            difficulties = [0] * MAX_N_TASKS
+            for index, d in enumerate(value):
+                if d.isdigit():
+                    difficulties[index] = int(d)
+                difficulty_map[key] = difficulties
+
+    result_table = []
+    with open(result_filename) as f:
+        for line in f:
+            s = line.strip()
+            m = re.match(re_contest_pattern, s)
+            if not m:
+                continue
+            key = m.groups()[0]
+            value = m.groups()[1]
+            result = [" "] * MAX_N_TASKS
+            for index, mark in enumerate(value):
+                result[index] = mark
+            result_table += [(key, result)]
+
+    result_table.sort(key=lambda x: x[0])
+    wins = [0] * MAX_DIFFICULTY
+    losses = [0] * MAX_DIFFICULTY
+
+    result_lines = ""
+    for [key, result] in result_table:
+        m = re.match(re_id_pattern, key)
+        id = int(m.groups()[0])
+        if id < lower_id or id > upper_id:
+            continue
+
+        if not key in difficulty_map:
+            print("Warning: missing {}".format(key))
+            continue
+
+        difficulties = difficulty_map[key]
+        line = ""
+        for index, difficulty in enumerate(difficulties):
+            difficulty_str = str(difficulty)
+            mark = result[index]
+            result_str = "  "
+            if difficulty == 0:
+                pass
+            elif mark == "+":
+                wins[difficulty] = wins[difficulty] + 1
+                result_str = str(difficulty) + result[index]
+            elif mark != " ":
+                losses[difficulty] = losses[difficulty] + 1
+                result_str = str(difficulty) + result[index]
+            line += " " + result_str
+        result_lines += key + ":" + line + "\n"
+
+    n_wins = 0
+    n_losses = 0
+    summary_lines = "difficulty: win+loss=total\n"
+    for [g, [w, l]] in enumerate(zip(wins, losses)):
+        if g < 1 or (w + l) <= 0:
+            continue
+        s = str(g) + colors[g] + ": " + str(w) + "+" + str(l) + "=" + str(w + l)
+        n_wins = n_wins + w
+        n_losses = n_losses + l
+        summary_lines = summary_lines + s + "\n"
+
+    s = "total: " + str(n_wins) + "+" + str(n_losses) + "=" + str(n_wins + n_losses) + "\n"
+    summary_lines = summary_lines + s
+    return (result_lines, summary_lines)
+
+def parse_command_line():
+    parser = argparse.ArgumentParser()
+    # コンテスト番号の下限
+    parser.add_argument("--lower_id", dest="lower_id", type=int,
+                        default=0, help="Lowest contest ID")
+    # コンテスト番号の上限
+    parser.add_argument("--upper_id", dest="upper_id", type=int,
+                        default=1000000, help="Hight contest ID")
+    # 問題の難易度
+    # "ABC297:112 5" はコンテスト ABC297の1..5問目の難易度が
+    # 1(灰),1(灰),2(茶),不明,5(青) という意味。6問目以降は問題が無いか難易度が不明。
+    # :の直後にA問題の結果を置く。:と結果の間には何も書かない。
+    parser.add_argument("--difficulty_filename", dest="difficulty_filename", type=str,
+                        default="incoming_data/difficulty_auto_abc.csv", help="Textfile for difficulty of tasks")
+    # 問題を解いた結果
+    # "ABC297: ++ -" はコンテスト ABC297の1..5問目を
+    # 未回答、解けた、解けた、未回答、解けなかったという意味。6問目以降は未回答。
+    parser.add_argument("--result_filename", dest="result_filename", type=str,
+                        default="results/results.txt", help="Textfiles for result of tasks")
+    args = parser.parse_args()
+    return args
+
+def execute_all():
+    args = parse_command_line()
+    lower_id = args.lower_id
+    upper_id = args.upper_id
+    difficulty_filename = args.difficulty_filename
+    result_filename = args.result_filename
+
+    result_lines, summary_lines = collect_results(
+        lower_id = lower_id, upper_id = upper_id,
+        difficulty_filename = difficulty_filename, result_filename = result_filename)
+    print(result_lines)
+    print(summary_lines)
+
+if __name__ == "__main__":
+    execute_all()
