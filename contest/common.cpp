0639de8eb4919d179a0600ced7bb494764144ee9
zettsu-t <zettsu.tatsuya@gmail.com>
2023-10-27 13:39:30 +0100

    Add a programming competition template

---
+++ contest/common.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <atcoder/modint>
+
+namespace {
+    using ModInt [[maybe_unused]] = atcoder::modint998244353;
+    using Num [[maybe_unused]] = long long int;
+    using Vec [[maybe_unused]] = std::vector<Num>;
+    using Set [[maybe_unused]] = std::set<Num>;
+    using Mset [[maybe_unused]] = std::multiset<Num>;
+
+    template<typename T>
+    using Q [[maybe_unused]] = std::queue<T>;
+
+    template<typename T>
+    using PQ [[maybe_unused]] = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+}
+
+void solve(std::istream& is, std::ostream& os) {
+    Num n {0};
+    std::string s;
+    is >> n >> s;
+    os << s << n;
+}
+
+int main(void) {
+    solve(std::cin, std::cout);
+    return 0;
+}
